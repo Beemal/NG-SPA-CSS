b280358368d206831acb1149db9db415fe25afda
Bimal Parajuli <bimalparajulise@gmail.com>
2023-08-19 18:07:17 -0400

    Initial Code of NG CSS
    Initial Code of NG CSS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.less']
 })
 export class AppComponent {
-  title = 'NG-SPA-CSS';
+  title = 'Charlotte Software Solutions';
 }
